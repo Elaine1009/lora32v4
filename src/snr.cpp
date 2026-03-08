aa64864493bc51bd34ba6bc22c7279ff032fde46
huan2183 <huan2183@purdue.edu>
2026-03-08 11:34:21 -0400

    separate sstate and rstate. not necessary but doing it for consistency

--- src/snr.cpp
+++ src/snr.cpp
@@ -12,37 +12,37 @@ SX1262 radio = new Module(8, 14, 12, 13);
 int cycle = 0;
 
 void setup() {
-  Serial.begin(115200);
-  delay(2000);
+	Serial.begin(115200);
+	delay(2000);
 	// bottom 4 bytes of MAC address of chip
 	DEVICE_ID = (uint32_t)(ESP.getEfuseMac() & 0xFFFFFFFF);
 	Serial.printf("Device ID: %08X\n", DEVICE_ID);
-  Serial.println("Initializing LoRa...");
+	Serial.println("Initializing LoRa...");
 
-  int state = radio.begin(
-    915.0,  // frequency
-    125.0,  // bandwidth
-    12,     // spreading factor
-    8,      // coding rate
-    0x12,   // sync word
-    22,     // TX power
-    16,     // preamble length
-    1.6     // TCXO voltage
-  );
-  if (state != RADIOLIB_ERR_NONE) {
-    Serial.print("LoRa init failed, code ");
-    Serial.println(state);
-    while (true);
-  }
+	int state = radio.begin(
+		915.0,  // frequency
+		125.0,  // bandwidth
+		12,     // spreading factor
+		8,      // coding rate
+		0x12,   // sync word
+		22,     // TX power
+		16,     // preamble length
+		1.6     // TCXO voltage
+	);
+	if (state != RADIOLIB_ERR_NONE) {
+		Serial.print("LoRa init failed, code ");
+		Serial.println(state);
+		while (true);
+	}
 
-  radio.setDio2AsRfSwitch(); // asks for FEM_EN to enable
+	radio.setDio2AsRfSwitch(); // asks for FEM_EN to enable
 
 	pinMode(2, OUTPUT); // enables FEM_EN
-  digitalWrite(2, HIGH);
+	digitalWrite(2, HIGH);
 
 	radio.setPacketReceivedAction(onReceive);
 	radio.startReceive();
-  Serial.println("LoRa init success!");
+	Serial.println("LoRa init success!");
 }
 
 void onReceive() {
@@ -57,12 +57,12 @@ void loop() {
 		String inID = "";
 		int state = radio.readData(str);
 		int delimindex = str.indexOf(DELIM);
-		/*
+
 		Serial.print("Raw: '");
 		Serial.print(str);
 		Serial.print("' delim: ");
 		Serial.println(delimindex);
-		*/
+
 		if (str.length() == 0) { // if zero length string, then ignore it
 			radio.startReceive();
 			return;
@@ -80,32 +80,32 @@ void loop() {
 		} else { // no ID found, so we just print the whole message
 			payload = str;
 		}
-		if (state == RADIOLIB_ERR_NONE) {
+		if (rstate == RADIOLIB_ERR_NONE) {
 			Serial.print("Received: ");
 			Serial.print(payload);
 			Serial.print(", RSSI: ");
 			Serial.print(radio.getRSSI());
 			Serial.println(" dBm, ID: ");
 			Serial.println(inID);
-		} else if (state != RADIOLIB_ERR_RX_TIMEOUT) {
+		} else if (rstate != RADIOLIB_ERR_RX_TIMEOUT) {
 			Serial.print("Receive failed, code ");
-			Serial.println(state);
+			Serial.println(rstate);
 		}
 		radio.startReceive(); // rearm receive after receiving a packet
 	}
-  if (millis() - lastSend >= 5000) { // assuming 24k clock speed, 5 seconds 
+	if (millis() - lastSend >= 20000) { // A lot of seconds
 		lastSend = millis();
 		cycle++;
 		char buffer[50];
 		sprintf(buffer, "%08X%spacket #%d", DEVICE_ID, DELIM, cycle);
 		Serial.printf("Sending: %s\n", buffer);
 
-		int state = radio.transmit(buffer);
-		if (state == RADIOLIB_ERR_NONE) {
+		int sstate = radio.transmit(buffer);
+		if (sstate == RADIOLIB_ERR_NONE) {
 			Serial.println("Packet sent!");
 		} else {
 			Serial.print("Send failed, code ");
-			Serial.println(state);
+			Serial.println(sstate);
 		}
 
 		radio.startReceive(); // start receiving after sending
